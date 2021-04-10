538fc9698eba3433d231eed9fb4a74c45fa664d3
Ariel Martínez Silberstein <amartinez5@uc.cl>
2021-04-10 00:01:30 -0400

    Use vuepress-homepage template

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,70 +1,46 @@
-const { description } = require('../../package')
-
 module.exports = {
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#title
-   */
-  title: 'Vuepress Docs Boilerplate',
-  /**
-   * Ref：https://v1.vuepress.vuejs.org/config/#description
-   */
-  description: description,
+  title: "Harry Potter",
+  description: "The description of the site.",
+  head: [["link", { rel: "icon", href: `/logo.png` }]],
+  base: "/",
+  dest: "./dist",
 
-  /**
-   * Extra tags to be injected to the page HTML `<head>`
-   *
-   * ref：https://v1.vuepress.vuejs.org/config/#head
-   */
-  head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
-  ],
-
-  /**
-   * Theme configuration, here is the default theme configuration for VuePress.
-   *
-   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
-   */
   themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
+    search: false,
     nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
+      { text: "Home", link: "/" },
+      { text: "About", link: "/about/" },
+      { text: "Projects", link: "/projects/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "GitHub", link: "https://github.com/mtobeiyf/vuepress-homepage" }
     ],
     sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
-        }
-      ],
-    }
+      '/guide/': genSidebarConfig('Guide')
+    },
+    lastUpdated: 'Last Updated'
   },
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
+  markdown: {
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+    extendMarkdown: md => {
+      md.use(require("markdown-it-katex"));
+    }
+  }
+};
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'customize',
+        'advanced',
+      ]
+    }
   ]
 }
+
